7285c17b75ab194332ffc252af3780fc0cb4f939
Harsha Naidu <harsha4.naidu@gmail.com>
2021-02-26 13:34:15 -0800

    added cors.rb and serializer

--- biddr_api/biddr_api/app/controllers/api/v1/auctions_controller.rb
+++ biddr_api/biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AuctionsController < Api::ApplicationController
 
-    before_action :find_auction, only:[:show, :destroy,:update]
+    before_action :find_auction, only:[:show]
     before_action :authenticate_user!, except: [:index, :show,:create]
 
     def index 
@@ -29,25 +29,7 @@ class Api::V1::AuctionsController < Api::ApplicationController
         end
     end
 
-    def update
-        if @auction.update auction_params
-            render json: {id: @auction.id}
-        else
-            render(
-                json: {errors: @auction.errors},
-                status: 422
-            )
-        end
-    end
     
-    def destroy
-        if @auction.destroy
-            head :ok
-        else
-            head :bad_request
-        end
-    end
-
     private
     def find_auction
         @auction=Auction.find params[:id]
